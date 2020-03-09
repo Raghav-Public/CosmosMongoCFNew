cc916f255ce54f2aa7ef0ceeb0fed782c3792c60
Raghav Darisi <raghav.darisi@pimcore.com>
2020-03-09 15:30:29 -0500

    added Transformation logic

---
+++ CosmosMongoCFNew/SourceTransformation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace CosmosMongoCFNew
+{
+    public class SourceTransformation
+    {
+        //{'key':'postDate', 'type':'date', 'fields':['YEAR', 'MONTH'] }
+        public string Key { get; set; }
+        public string Format { get; set; }
+        public string TransformationType { get; set; }
+
+        public SourceTransformation() { }
+
+        public string Execute(MongoDB.Bson.BsonDocument _doc)
+        {
+            if (TransformationType.ToUpper() == "DATE")
+            {
+                return GetDateTransformedString(_doc);
+            }
+            return "";
+        }
+
+        private string GetDateTransformedString(MongoDB.Bson.BsonDocument _doc)
+        {
+            DateTime bdate = _doc.GetValue(Key).AsDateTime;
+            return bdate.ToString(Format);
+        }
+    }
+}
