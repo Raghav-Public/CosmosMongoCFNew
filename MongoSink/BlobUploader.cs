560ee8208365ce35a3c7b80a7578fae1ca5b0184
Raghav Darisi <raghav.darisi@pimcore.com>
2020-03-23 12:55:49 -0500

    updated

--- MongoSink/BlobUploader.cs
+++ MongoSink/BlobUploader.cs
@@ -24,17 +24,22 @@ namespace MongoSink
         {
             try
             {
+                string filename = Guid.NewGuid().ToString() + ".json";
+                Console.WriteLine("writing failed data to:" + filename);
                 CloudStorageAccount storageAccount = CloudStorageAccount
                                                         .Parse(this.BlobConnectionString);
                 CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
                 CloudBlobContainer container = blobClient.
                                                     GetContainerReference(this.BlobContainer);
+                container.CreateIfNotExists();
                 CloudBlockBlob blockBlob = container.
-                                            GetBlockBlobReference(Guid.NewGuid().ToString() + ".json");
-                blockBlob.UploadText(data);
+                                            GetBlockBlobReference(filename);
+                blockBlob.UploadText(data, Encoding.UTF8);
+                Console.WriteLine("wrote to blob");
             }
             catch(Exception exp)
             {
+                Console.WriteLine(exp);
                 throw exp;
             }
         }
