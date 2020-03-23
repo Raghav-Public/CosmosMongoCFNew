63bead2229c2914d339bc226f4ade0ca58d3038d
Raghav Darisi <raghav.darisi@pimcore.com>
2020-03-23 12:56:23 -0500

    newcode

---
+++ CosmosMongoCFNew/Transformations/Filter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmosMongoCFNew.Transformations
+{
+    public abstract class Filter
+    {
+        public abstract string Type { get; }
+        public abstract void Execute(Object obj);
+    }
+}
